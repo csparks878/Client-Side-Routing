65f8ffcf5069bac53d58ab257992150c13aec11c
Charlie Sparks <Charlie@Charlies-MacBook-Pro.local>
2018-02-14 15:58:24 -0800

    not working but taking a break so I'm committing

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route} from "react-router-dom";
 
 import './index.css';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie'
 
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+
+    }
+  }
+
+  render(){
+    return(
+      <div className="App">
+        <Route path="/" exact component={MovieList} />
+        <Route path="/movies/{id}" component={Movie} />
+      </div>
+    )
+  }
+}
+
 ReactDOM.render(
-  <div>Application running, add your routing</div>,
+  <Router>
+    <App />
+  </Router>,
   document.getElementById('root')
 );
